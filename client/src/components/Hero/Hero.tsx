3aabf714ebafae52e0e8d673b9abcaf7314a1dc8
Michiel <47319476+mvdgragt@users.noreply.github.com>
2025-04-09 13:35:23 +0200

    updated file locations hero image

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,23 +1,23 @@
+import heroImage from "../assets/804.25102-Header_Pasen_04_Desktop.png";
+import logoImage from "../assets/Logo_Bakverkstan_Sedan2023_transparent.webp";
+
 const Hero = () => {
   return (
     <div className="h-screen w-screen relative">
-      {/* Image container with dark overlay */}
       <div className="absolute inset-0">
         <img
-          src="src/assets/804.25102-Header_Pasen_04_Desktop.png"
+          src={heroImage}
           alt="Hero"
           className="w-full h-full object-cover"
         />
-        {/* Dark overlay */}
         <div className="absolute inset-0 bg-black/40"></div>
       </div>
 
-      {/* Centered logo */}
       <div className="relative h-full flex items-center justify-center">
         <img
-          src="src/assets/Logo_Bakverkstan_Sedan2023_transparent.webp"
+          src={logoImage}
           alt="Bakverkstan Logo"
-          className="z-10 w-64 md:w-96" // Adjust the width (w-64 md:w-96) as needed for your logo
+          className="z-10 w-64 md:w-96"
         />
       </div>
     </div>
